6d26636f26ef68faff3791a9091bad2220402fa8
sh1mj1 <7wlgns@gmail.com>
2024-04-30 21:04:50 +0900

    style: kt format에 맞게 수정
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/movie/domain/model/Theaters.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Theaters.kt
@@ -3,9 +3,10 @@ package woowacourse.movie.domain.model
 data class Theaters(val theaters: List<Theater>) {
     constructor(vararg theaters: Theater) : this(theaters.toList())
 
-    fun screeningTheater(screen: Screen): Theaters = Theaters(
-        theaters.filter { theater ->
-            theater.screens.any { it.id == screen.id }
-        },
-    )
+    fun screeningTheater(screen: Screen): Theaters =
+        Theaters(
+            theaters.filter { theater ->
+                theater.screens.any { it.id == screen.id }
+            },
+        )
 }
